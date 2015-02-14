8f6bd16d033c0fd1963da242556c499f7fbb8ed3
Konrad Welc <konrad1331@gmail.com>
2015-02-15 00:51:41 +0100

    It's ALIVE !!!!! aka avoid works
    some code cleaning and finished avoid

--- AI/AI/SteeringBehaviour.cpp
+++ AI/AI/SteeringBehaviour.cpp
@@ -8,7 +8,7 @@ wander_on(0), seek_on(0), flee_on(0), arrive_on(0), obstacle_avoidance_on(0), sc
 	// ---------- WANDER INIT ---------- //
 	wander_radius	= 3.0f;
 	wander_distance = 5.0f;
-	wander_jitter	= 1.f;
+	wander_jitter	= 1.2f;
 	float alpha = (float)(rand()%360) * (float)(PI/180.0f);
 	wander_target = glm::vec2(wander_radius*cos(alpha), wander_radius*sin(alpha));
 	wander_target_point.InitPoint(wander_target, 0.2, glm::vec3(0, 0, 1));
@@ -47,19 +47,19 @@ glm::vec2 SteeringBehaviour::CalculateSteeringForce(void)
 	glm::vec2 force_hide = CalculateHide();
 	steering_force = glm::vec2(0.0f, 0.0f);
 
-	steering_force += force_seek;
-	return steering_force;
-/*
+	
+
 	if(GetLength(force_oa) > 0)
 	{
-		steering_force += force_oa * 1.f;
-		//steering_force += force_seek * 0.2f;
+		steering_force += force_oa * 0.8f;
+		steering_force += force_seek * 0.2f;
 		return steering_force;
 	}
 	else
 	{
-		
-	}*/
+		steering_force += force_seek;
+		return steering_force;
+	}
 }
 
 void SteeringBehaviour::Draw(void)
@@ -94,7 +94,7 @@ glm::vec2 SteeringBehaviour::CalculateWander(void)
 
 	wander_target_point.UpdatePoint(target_world, 0.2, glm::vec3(0, 0, 1));
 
-	return target_world - owner->object_position;
+	return (target_world - owner->object_position)*glm::vec2(3333,3333);
 }
 
 glm::vec2 SteeringBehaviour::CalculateSeek(const glm::vec2 &target)
@@ -129,15 +129,16 @@ glm::vec2 SteeringBehaviour::CalculateArrive(void)
 glm::vec2 SteeringBehaviour::CalculateObstacleAvoidance(void)
 {
 	GameEntity *object = 0;
-	float detection_box_length = (GetLength(owner->object_velocity)/ZOMBIE_MAX_SPEED);
+	float detection_box_length = (GetLength(owner->object_velocity)/ZOMBIE_MAX_SPEED);	
 	detection_box_length *=	MIN_DETECTION_BOX_LENGTH;
 	detection_box_length += MIN_DETECTION_BOX_LENGTH;
 	float distance = 0.0f;
+	float dist_to_closest_ip = 9999.0f;
 
-	float dist_to_closest_ip = 999999.0f;
 	GameEntity *pointer_to_closest_ob = 0;
 	glm::vec2 local_position_of_closest_ob = glm::vec2(0, 0);
 
+	//debug
 	obstacle_number = 0;
 	for(int i=0; i<obstacle_number; ++i)
 		obstacle_position[i].Hide();
@@ -145,70 +146,75 @@ glm::vec2 SteeringBehaviour::CalculateObstacleAvoidance(void)
 	for(int i=0; i<intersection_number; ++i)
 		intersection[i].Hide();
 
-	obstacle_x_axis.UpdateLine(owner->GetObjectPosition() - GetPerpendicular(owner->object_heading) * glm::vec2(33, 33), owner->GetObjectPosition() + GetPerpendicular(owner->object_heading) * glm::vec2(33, 33), glm::vec3(0, 1, 0));
-	obstacle_y_axis.UpdateLine(owner->GetObjectPosition() - owner->GetObjectHeading() * glm::vec2(33, 33), owner->GetObjectPosition() + owner->GetObjectHeading()  * glm::vec2(33, 33), glm::vec3(0, 1, 0));
+	//debug
+	obstacle_x_axis.UpdateLine(owner->GetObjectPosition() - GetPerpendicular(owner->object_heading) * glm::vec2(2, 2), owner->GetObjectPosition() + GetPerpendicular(owner->object_heading) * glm::vec2(2, 2), glm::vec3(0, 1, 0));
+	obstacle_y_axis.UpdateLine(owner->GetObjectPosition() - owner->GetObjectHeading() * glm::vec2(3, 3), owner->GetObjectPosition() + owner->GetObjectHeading()  * glm::vec2(7, 7), glm::vec3(0, 1, 0));
 	obstacle_box.UpdateRectangle(glm::vec2(0.f, 0.f), glm::vec2(1.f, detection_box_length), glm::vec3(0, 1, 0));
 
 	float angle = GetAngle(glm::vec2(0, 1), owner->object_heading);
 	
-	for(unsigned int i=0; i<owner->scene->objects.size(); ++i)
+	for(unsigned int i=0; i<owner->scene->obstacles.size(); ++i)
 	{
-		object = owner->scene->objects[i];
+		object = owner->scene->obstacles[i];
 
-		if(object == owner || object == owner->scene->player)
-			continue;
+		//if dist(obj.pos + zom.pos) > detectionBox + obj.rad -> skip
+		if ((GetDistance(object->GetObjectPosition(), owner->GetObjectPosition())) > detection_box_length+ object->GetCollisionRadius()) continue;	
 
-
-		if ((GetDistance(object->GetObjectPosition(), owner->GetObjectPosition())) > detection_box_length+ object->GetCollisionRadius()){
-			//cout << GetDistance(object->GetObjectPosition(), owner->GetObjectPosition()) + object->GetCollisionRadius() << ", " << detection_box_length << "\n";
-			continue;
-		}
-
+		//ToLocalSpace
 		glm::mat4 rot = glm::rotate(glm::mat4(1.0f), (float)(-angle), glm::vec3(0, 0, 1));
 		glm::vec2 local_position(rot * glm::vec4(object->GetObjectPosition() - owner->GetObjectPosition(), 0.0f, 0.0f));
-		
-		if (local_position.y < 0)
-			continue;		
 
+		//if behind -> skip
+		if (local_position.y < 0) continue;		
+
+		//debug
 		obstacle_position[obstacle_number].Hide();
 		obstacle_position[obstacle_number++].UpdatePoint(local_position, object->GetCollisionRadius(), glm::vec3(0, 1, 0));
 
-
+		//obj.rad + zom.rad
 		float expanded_radius = object->GetCollisionRadius() + owner->GetCollisionRadius();
 
-		if (fabs(local_position.x) > expanded_radius)
-			continue;
-
-		cout << expanded_radius << endl;
+		//if doesn't collide -> skip
+		if (fabs(local_position.x) >= expanded_radius) continue;		
 
 		double cX = local_position.x;
 		double cY = local_position.y;
 
 		double sqrt_part = sqrt(expanded_radius*expanded_radius - cX*cX);
 
+		//intersection point
 		double ip = cY - sqrt_part;
 		if(ip <= 0) ip = cY + sqrt_part;
 
+		//debug
 		intersection[intersection_number].Hide();
 		intersection[intersection_number++].UpdatePoint(glm::vec2(0, ip) , 0.2f, glm::vec3(1, 0, 0));
 
 		if(ip < dist_to_closest_ip)
 		{
 			dist_to_closest_ip = (float)ip;
-			pointer_to_closest_ob = object;
-			local_position_of_closest_ob = local_position;
+			pointer_to_closest_ob = object;					//rember closest obj
+			local_position_of_closest_ob = local_position;	//rember closest obj position in LocalSpace
 		}
 	}
 
 	glm::vec2 force(0.0f, 0.0f);
 
-	if(pointer_to_closest_ob == 0)
-		return force;
+	//if doesnt have IP return 0 force
+	if(pointer_to_closest_ob == 0) return force;
 
+	//stronger force the closer we are
 	double multiplier = 1.0 + (detection_box_length - local_position_of_closest_ob.y) / detection_box_length;
+	
+	//if negative -> collapses zombie behaviour
+	if (multiplier < 0) multiplier = 0.1;
 
-	// lateral force
-	force.x = (float)((pointer_to_closest_ob->GetCollisionRadius() - local_position_of_closest_ob.x) * multiplier);
+	// lateral force with sides
+	if (local_position_of_closest_ob.x<0)
+		force.x = (float)((pointer_to_closest_ob->GetCollisionRadius() - local_position_of_closest_ob.x) * multiplier);
+	else
+		force.x = -(float)((pointer_to_closest_ob->GetCollisionRadius() + local_position_of_closest_ob.x) * multiplier);
+
 
 	const double braking_weight = 0.2;
 
@@ -217,7 +223,7 @@ glm::vec2 SteeringBehaviour::CalculateObstacleAvoidance(void)
 	glm::mat4 rot = glm::rotate(glm::mat4(1.0f), GetAngle(glm::vec2(0, 1), owner->object_heading), glm::vec3(0, 0, 1));
 	glm::vec2 force_world(rot * glm::vec4(force, 0.0f, 0.0f));
 
-	return force_world;
+	return force_world*glm::vec2(333,333);
 }
 
 glm::vec2 SteeringBehaviour::CalculateHidingSpot(const glm::vec2 &target, const glm::vec2 &obstacle, double radius)
